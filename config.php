154a4cf6994bef1306e697d29961c4879caec974
AhmadBB <90705524+ahmadktn@users.noreply.github.com>
2025-08-06 20:43:55 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
 
-$host = 'localhost';
+$host = '161.97.84.20';
 $dbname = 'fxminer';
-$username = 'root';
-$password = '';
+$username = 'user';
+$password = 'fxminerweb123';
+$port = 3309;
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
